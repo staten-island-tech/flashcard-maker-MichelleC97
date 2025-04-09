9bc513e610ab67865e4a3e1121f5d5d8fffaed07
MichelleC97 <MIchellec401@nycstudents.net>
2025-04-09 10:12:19 -0400

    commit

---
+++ epi.py
@@ -0,0 +1,13 @@
+disease = int(input("people_infected: "))
+P = 750
+N = 1
+R = 5
+
+while people_infected > 1:
+    for [int(input(1))] (f"person_infected") + 5 (f"people_infected"):
+    people_infected = people_infected + R
+    days = days + 1
+    if people_infected > 750:
+        break
+
+print[int(input("days: "))]
