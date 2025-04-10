55a7b4a5be5b2a83d2a0f88e4159d65c2857965a
MichelleC97 <MIchellec401@nycstudents.net>
2025-04-10 10:17:11 -0400

    c

---
+++ Flashcard(Students).py
@@ -0,0 +1,10 @@
+import json
+json_String = '(f"Word":" ", "Definition": " ")'
+data_from_string = json.loads(json_String)
+
+student: (f"answer_question")
+input(json(f"word: "))
+
+print(f"definition:")
+
+for every_correct
