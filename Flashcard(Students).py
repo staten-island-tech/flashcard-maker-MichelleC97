387b2e7353fcd72e1c2b652ceb027b9bcd053a37
MichelleC97 <MIchellec401@nycstudents.net>
2025-04-22 10:03:36 -0700

    FINALLY DONE

--- Flashcard(Students).py
+++ Flashcard(Students).py
@@ -1,13 +1,14 @@
 import json
 
 FILENAME = "flashcards.json"
-
+    
 def load_flashcards():
     try:
         with open(FILENAME, "r") as file:
-            return json.load(file)
+            flashcards = json.load(file)
+            return flashcards if isinstance(flashcards, dict) else {}
     except FileNotFoundError:
-        return []
+        return {}
     
 def save_flashcards(flashcards):
     with open(FILENAME, "w") as file:
@@ -18,7 +19,7 @@ def teacher_mode():
     print("Enter your Flashcard (leave question blank to stop):")
     while True:
         question = input("Enter the word/phrase/question:")
-        if question() == "":
+        if question == "":
             break
         answer = input("Enter the answer: ")
         flashcards[question] = answer
@@ -29,19 +30,19 @@ def teacher_mode():
 def student_mode():
     flashcards = load_flashcards()
     if not flashcards:
-        print("No flashcards fond. Teacher need make.")
+        print("No flashcards found. Please add some in teacher mode.")
         return
 
     print("Starting Quiz. 'exit' to stop.")
-    question= list(flashcards.items())
+    flashcard_list = list(flashcards.items())      
     score = 0
     streak = 0
 
-    for question, correct_answer in question:
-        answer = input(f"{question} = ")
+    for q, correct_answer in flashcard_list:        
+        answer = input(f"{q} = ").strip()
         if answer.lower() == "exit":
             break
-        if answer.lower == correct_answer.lower():
+        if answer.lower() == correct_answer.lower():
             streak += 1
             bonus = (streak - 1)
             score+= 1 + bonus
